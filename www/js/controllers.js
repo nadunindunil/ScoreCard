e85a5be50519bb8799f1e23fa5b16363ca8f70a1
nadunindunil <nadun1indunil@gmail.com>
2015-09-29 19:38:09 +0530

    added notifications
    notification page and routing is done.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,7 +36,57 @@ app.controller('DataEntryCtrl', function($scope,  Authorization) {
   $scope.input = Authorization;
 });
 
-app.controller('ScoreSheetCtrl', function($scope, Authorization) {
+//app.controller('ScoreSheetCtrl', function($scope, Authorization) {
+//  $scope.input = Authorization;
+//  var v1 = $scope.val1= 0;
+//  var v2 = $scope.val2= 0;
+//
+//  $scope.increment1=function(){
+//    //value = value + 1;
+//    //v1=v1+1;
+//    $scope.val1++;
+//
+//  };
+//  $scope.increment2=function(){
+//    $scope.val2++;
+//
+//  };
+//
+//  $scope.decrement1=function(){
+//    if($scope.val1 > 0){
+//      $scope.val1--;
+//    }
+//
+//
+//  };
+//
+//  $scope.decrement2=function(){
+//    if($scope.val2 > 0){
+//      $scope.val2--;
+//    }
+//  };
+//
+//  $scope.init = function () {
+//    if (val1 >= 11 || val2 >= 11) {
+//      if (val1 - val2 > 1) {
+//        // player one won!
+//      }
+//      else {
+//        // player two won!
+//      }
+//    }
+//  };
+//
+//
+//
+//});
+
+
+app.controller('ScoreSheetCtrl', function($scope, $ionicModal,Authorization) {
+  console.log('ScoreSheetCtrl');
+
+
+
   $scope.input = Authorization;
   var v1 = $scope.val1= 0;
   var v2 = $scope.val2= 0;
@@ -45,10 +95,12 @@ app.controller('ScoreSheetCtrl', function($scope, Authorization) {
     //value = value + 1;
     //v1=v1+1;
     $scope.val1++;
+    init();
 
   };
   $scope.increment2=function(){
     $scope.val2++;
+    init();
 
   };
 
@@ -65,4 +117,43 @@ app.controller('ScoreSheetCtrl', function($scope, Authorization) {
       $scope.val2--;
     }
   };
+
+  init();
+
+
+  ///////////////////////////////////////////////////
+  $ionicModal.fromTemplateUrl('partials/myForm.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function(){
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function(){
+    $scope.modal.hide();
+  };
+
+  function init () {
+    if ($scope.val1 >= 11 || $scope.val2 >= 11) {
+      if ($scope.val1 - $scope.val2 > 1) {
+        // player one won!
+
+        $scope.modal.show();
+
+      }
+      else if ($scope.val2 - $scope.val1 > 1) {
+        // player two won!
+
+        $scope.modal.show();
+      }
+    }
+  }
+
+
+
+
 });
